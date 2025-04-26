29e3d3e43b6a048c815de3313ff290bbe9826ed9
samriddha roy chowdhury <roychowdhurysamriddha@gmail.com>
2025-04-26 15:44:19 +0530

    added styling and signup page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-// component with header and sub header together
-// TODO: add tcss
-const Header = ({heading, subheading}) => {
+
+const Header = ({ heading, subheading }) => {
   return (
-    <div>
-      <h3>{heading}</h3>
-      <h5>{subheading}</h5>
+    <div className="text-center mb-6">
+      <h3 className="text-2xl font-bold text-gray-800">{heading}</h3>
+      <h5 className="text-sm text-gray-500 mt-2">{subheading}</h5>
     </div>
   )
 }
