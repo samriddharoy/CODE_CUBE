fb4dc65e166897df2ec128e8893c3b631e2f0f5b
preyum <mfpriyam@gmail.com>
2025-04-29 21:39:52 +0530

    add: styles, color pallette and some clean-up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Header = ({ heading, subheading }) => {
   return (
     <div className="text-center mb-6">
-      <h3 className="text-2xl font-bold text-gray-800">{heading}</h3>
-      <h5 className="text-sm text-gray-500 mt-2">{subheading}</h5>
+      <h3 className="text-2xl font-bold text-vanilla">{heading}</h3>
+      <h5 className="text-sm text-vanilla/80 mt-2">{subheading}</h5>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
