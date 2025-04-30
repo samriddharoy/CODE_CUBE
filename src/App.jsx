0c182cd86a04a8e3a392bb0bd0a25451e6e33d27
preyum <mfpriyam@gmail.com>
2025-04-30 15:54:39 +0530

    add: form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Signup from "./pages/Signup"
-
+import Signin from "./pages/Signin"
 function App() {
-  return<div>
-    <Signup />
-  </div>          
+  return(
+  <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<Home />}></Route> */}
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signin" element={<Signin />}></Route>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App
